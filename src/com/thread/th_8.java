b3ba4f0c0186e79520820bcd96547ebc2f9ca065
Shi Jiajun <1178972961@qq.com>
2023-02-22 21:08:11 +0800

    2.22 产生死锁的一个例子
    会产生死锁的一个例子

--- src/com/thread/th_8.java
+++ src/com/thread/th_8.java
@@ -1,4 +1,5 @@
 package com.thread;
+
 // 可重入锁 & 死锁
 // 可重入锁：对同一个线程，获取到一个锁后还能继续获取同一个锁（因为内部函数调用）
 // 因为可重入，synchronized 不仅要记录是否加锁，也要记录加了几次锁。
@@ -6,11 +7,18 @@ package com.thread;
 public class th_8 {
     public static void main(String[] args) {
         Counter3.add(10);
+
+        // 死锁的一个例子
+
     }
 }
 
 public class Counter3 {
+    public static final Object LockA = new Object();
+    public static final Object LockB = new Object();
+
     private int count = 0;
+    private int another = 0;
 
     public synchronized void add(int n) {   //  main 线程第一次获得锁
         if (n < 0) {
@@ -22,5 +30,25 @@ public class Counter3 {
 
     public synchronized void dec(int n) {
         count -= n;
+    }
+
+
+    // 以下两个函数是死锁的一个实例.由于在各自占有一个锁以后请求对方的锁，导致无限等待
+    public void abc() {
+        synchronized (LockA) {
+            count += 5;
+            synchronized (LockB) {
+                another += 10;
+            }
+        }
+    }
+
+    public void xyz() {
+        synchronized (LockB) {
+            another -= 6;
+            synchronized (LockA) {
+                count -= 15;
+            }
+        }
     }
 }
