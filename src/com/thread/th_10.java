60bb57fa9ba2406c1dfe97b845bc62255b5bb66b
Shi Jiajun <1178972961@qq.com>
2023-02-25 16:32:17 +0800

    2.25 ReentrantLock 的 tryLock
    tryLock 可以在等待一定时间无果后放弃，而不必陷入长久的等待。

--- src/com/thread/th_10.java
+++ src/com/thread/th_10.java
@@ -1,10 +1,12 @@
 package com.thread;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 // ReentrantLock （可重入锁）是 synchronized 的更好的替代品
-// synchronized 太重了，而且获取时必须一直等待，没有额外的尝试机制
+// synchronized 太重了，而且获取时必须一直等待，不能选择【在等待一定时间后放弃】
+// ReentrantLock 必须先获取到锁，再进入 try {...} 代码块，最后使用 finally 保证释放锁；
 public class th_10 {
 }
 
@@ -22,12 +24,14 @@ class Counter5 {
     // ReentrantLock 写法,需要手动处理异常
     // 基本就和 Go 很像了，临界区代码前后是 Lock / Unlock
     // 和 synchronized 不同的是，ReentrantLock 可以尝试获取锁：
-    public void add(int n) {
-        lock.lock();
-        try {
-            count += n;
-        } finally {
-            lock.unlock();
+    public void add(int n) throws InterruptedException {
+        if(lock.tryLock(1, TimeUnit.SECONDS)){  // 尝试获取锁的时候，最多等待1秒。如果1秒后仍未获取到锁，tryLock()返回false，程序放弃等待获取锁，就可以做一些额外处理，而不是无限等待下去。
+            try {
+                count += n;
+            } finally {
+                lock.unlock();
+            }
         }
+
     }
 }
