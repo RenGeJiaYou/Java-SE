c0e5ca84d864673a5ee41a54a47a425366ebf6bb
Shi Jiajun <1178972961@qq.com>
2023-02-25 10:40:36 +0800

    2.25 修改：使用 wait() / notify() 正确调度线程

--- src/com/thread/th_9.java
+++ src/com/thread/th_9.java
@@ -3,8 +3,9 @@ package com.thread;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// 使用 wait 和 notify
+// 使用 wait 和 notify 实现任务的等待和唤醒
 // synchronized 让多线程间实现了同步，但是还不够协调
+// 多线程协调的原则：当条件不满足时，线程进入「等待」状态；当条件满足时，线程被「唤醒」，继续执行任务。
 public class th_9 {
     public static void main(String[] args) {
 
@@ -17,12 +18,14 @@ class TaskQueue{
     // 添加元素前加锁，完成后解锁
     public synchronized void addTask(String s){
         this.queue.add(s);
+        this.notify();  // 满足条件后，唤醒在等待 this 锁的线程
     }
 
-    public synchronized  String getTask() {
+    public synchronized  String getTask() throws InterruptedException {
         // while 的意图为：循环检测 queue 的状态，为空就继续等待
-        // 但实际上，while() 永远不会退出，因为在 getTask() 已经得到了当前对象的 this 锁。其它线程无法调用 addTask() 方法，因为 addTask() 也要调用 this 锁
         while (queue.isEmpty()) {
+            this.wait();    // this 锁被释放。当条件不满足时，本线程等待。
+            // 重新获得锁。线程在等待状态时 wait() 不会返回，而在将来被某个进程唤醒后才会返回。
         }
         return queue.remove();
     }
