14536b32646d94e30e7453686e4558126812998d
Shi Jiajun <1178972961@qq.com>
2023-02-28 14:11:12 +0800

    2.28 多个 CompletableFuture 的并行执行
    1. anyOf() 要求传入的所有 CompletableFuture 只需成功一个
    2. allOf() 要求传入的所有都得成功

--- src/com/thread/th_19.java
+++ src/com/thread/th_19.java
@@ -23,21 +23,94 @@ public class th_19 {
 //        // 主线程不要立刻结束，否则 CompletableFuture 默认使用的线程池会立刻关闭:
 //        Thread.sleep(2000);
 
-        // 二、多个 CompletableFuture 可以串行执行
+
+        // 二、多个 CompletableFuture 串行执行
         // 例：第一个实例根据证券名称查找股票代码，第二个实例根据股票代码返回股票价格。串行操作
         // 第一个任务:
-        CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> queryCode("中国石油"));
+//        CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> queryCode("中国石油"));
+//
+//        // cf1成功后继续执行cf2:
+//        CompletableFuture<Double> cf2 = cf1.thenApplyAsync((code) -> fetchPrices(code));
+//
+//        // cf2 成功后打印结果
+//        cf2.thenAccept((res) -> {
+//            System.out.println("price: " + res);
+//        });
+//
+//        // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
+//        Thread.sleep(2000);
 
-        // cf1成功后继续执行cf2:
-        CompletableFuture<Double> cf2 = cf1.thenApplyAsync((code) -> fetchPrices(code));
 
-        // cf2 成功后打印结果
-        cf2.thenAccept((res) -> {
-            System.out.println("price: " + res);
+        // 三、多个 CompletableFuture 并行执行
+        // 多个 queryCode 操作或多个 fetchPrices 内可以异步执行。而在 queryCode 和 fetchPrices 间是同步操作
+        // 1. 两个 CompletableFuture 执行异步 queryCode1 :
+        CompletableFuture<String> cf_sina = CompletableFuture.supplyAsync(() -> {
+            // 再重复一次，语法上是传入了一个函数，实际是通过 lambda 简化的一个类对象，这个对象实现了一个单方法接口
+            return queryCode1("中国石油", "https://finance.sina.com.cn/code/");
         });
-        
-        // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
+
+        CompletableFuture<String> cf_163 = CompletableFuture.supplyAsync(() -> {
+            return queryCode1("中国石油", "https://money.163.com/code/");
+        });
+
+        // 2.用 anyOf 合并为一个新的 CompletableFuture:
+        CompletableFuture<Object> cf_any = CompletableFuture.anyOf(cf_sina, cf_163);
+
+        // 3.两个 CompletableFuture 执行异步 fetchPrice1 :
+        CompletableFuture<Double> cf_sina_1 = cf_any.thenApplyAsync((code) -> {
+            return fetchPrice1((String) code, "https://finance.sina.com.cn/price/");
+        });
+
+        CompletableFuture<Double> cf_163_1 = cf_any.thenApplyAsync((code) -> {
+            return fetchPrice1((String) code, "https://money.163.com/price/");
+        });
+
+        // 4.用 anyOf 合并为一个新的 CompletableFuture:
+        CompletableFuture<Object> cf_any_1 = CompletableFuture.anyOf(cf_sina_1,cf_163_1);
+
+        // 5.最终结果
+        cf_any_1.thenAccept((res)->{
+            System.out.println("price: "+res);
+        });
+
         Thread.sleep(2000);
+
+//        上述逻辑实现的异步查询规则实际上是：
+//
+//┌─────────────┐ ┌─────────────┐
+//│ Query Code  │ │ Query Code  │
+//│  from sina  │ │  from 163   │
+//└─────────────┘ └─────────────┘
+//       │               │
+//       └───────┬───────┘
+//               ▼
+//        ┌─────────────┐
+//        │    anyOf    │
+//        └─────────────┘
+//               │
+//       ┌───────┴────────┐
+//       ▼                ▼
+//┌─────────────┐  ┌─────────────┐
+//│ Query Price │  │ Query Price │
+//│  from sina  │  │  from 163   │
+//└─────────────┘  └─────────────┘
+//       │                │
+//       └────────┬───────┘
+//                ▼
+//         ┌─────────────┐
+//         │    anyOf    │
+//         └─────────────┘
+//                │
+//                ▼
+//         ┌─────────────┐
+//         │Display Price│
+//         └─────────────┘
+
+        // anyOf() 要求传入的所有 CompletableFuture 只需成功一个；allOf() 要求传入的所有都得成功
+        // thenAccept()处理正常结果；
+        // exceptional()处理异常结果；
+        // thenApplyAsync()用于串行化另一个 CompletableFuture；
+        // anyOf() 和 allOf() 用于并行化多个 CompletableFuture。
     }
 
     // 本静态方法的签名符合 Supplier 接口的定义（除了方法名外）。
@@ -65,6 +138,24 @@ public class th_19 {
     static Double fetchPrices(String code) {
         try {
             Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        return 5 + Math.random() * 20;
+    }
+
+    static String queryCode1(String name, String url) {
+        System.out.println("query code from " + url + "...");
+        try {
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (InterruptedException e) {
+        }
+        return "601857";
+    }
+
+    static Double fetchPrice1(String code, String url) {
+        System.out.println("query price from " + url + "...");
+        try {
+            Thread.sleep((long) (Math.random() * 100));
         } catch (InterruptedException e) {
         }
         return 5 + Math.random() * 20;
